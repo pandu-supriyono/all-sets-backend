d78783f6f233c796313ba67f3879985bf76c9751
Pandu Supriyono <mail@supriyono.nl>
2022-08-23 02:53:00 +0200

    chore: use postgresql in production

---
+++ config/env/production/database.js
@@ -0,0 +1,23 @@
+const parse = require('pg-connection-string').parse;
+
+module.exports = ({ env }) => {
+  const config = env('DATABASE_URL')
+
+  return {
+    connection: {
+      client: 'postgres',
+      connection: {
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false
+        },
+      },
+      debug: false,
+    },
+  }
+};
+
